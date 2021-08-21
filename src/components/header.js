f97ccabfc6323855fecb00c54d2e8a36c326a4dd
anton <demeshkevichanton@gmail.com>
2021-08-21 17:40:20 +0300

    refact

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,63 @@
 import * as React from "react"
 import PropTypes from "prop-types"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
+// import gatsby from '../images/gatsby-icon.png'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+const Header = ({ siteTitle }) => {
+  const site = useStaticQuery(
+    graphql`
+   query{
+  site {
+    siteMetadata {
+      title
+      author
+      description
+      siteUrl
+     
+    }
+  }
+}`
+  )
+  console.log("site", site)
+
+  return (
+
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`
+        }}
+      >
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`
+            }}
+          >
+            {siteTitle}
+            {/*<img src={gatsby} alt="gatsby image" />*/}
+          </Link>
+        </h1>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
+  siteTitle: PropTypes.string
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: ``
 }
 
 export default Header
