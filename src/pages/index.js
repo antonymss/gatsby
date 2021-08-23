b5ba0f1f58bc43783bd33b70e140580613a1fa14
anton <demeshkevichanton@gmail.com>
2021-08-23 11:10:29 +0300

    refact

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,56 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { Link, graphql } from "gatsby"
+import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image"
+
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const { nodes } = data.allMarkdownRemark
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <h1>Hi people</h1>
+      {/*<p>Welcome to your new Gatsby site.</p>*/}
+      {/*<p>Now go build something great.</p>*/}
+      {/*<StaticImage*/}
+      {/*  src="../images/gatsby-astronaut.png"*/}
+      {/*  width={300}*/}
+      {/*  quality={95}*/}
+      {/*  formats={["AUTO", "WEBP", "AVIF"]}*/}
+      {/*  alt="A Gatsby astronaut"*/}
+      {/*  style={{ marginBottom: `1.45rem` }}*/}
+      {/*/>*/}
+      <div>
+        {nodes.map(post => {
+          const { category, title, url, image } = post.frontmatter
+          const img = getImage(image)
+          return (
+            <div key={post.id}>
+              <GatsbyImage alt={title} image={img} />
+              <Link to={`/${category}/${url}`} >{title}</Link>
+            </div>
+          )
+        })}
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query MainPage{
+   allMarkdownRemark {
+    nodes {
+      frontmatter {
+        title
+        category
+        url
+      }
+      id
+    }
+  }
+}
+`
